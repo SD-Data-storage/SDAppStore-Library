a987208a36e865a600ac3ba3ae2c55f0923d34d5
Alex <142623358+Alexidians@users.noreply.github.com>
2026-08-08 19:55:44 +0300

    Enhance VLC installation process on Windows
    Updated VLC installation script to copy extracted contents and clean up temporary files.

--- packages/vlc_media_player/install_windows.py
+++ packages/vlc_media_player/install_windows.py
@@ -3,8 +3,9 @@ import platform
 import re
 import urllib.request
 import sys
+import shutil
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-from sdpkg import download_file, fetch_contents, extract_zip
+from sdpkg import download_file, fetch_contents, extract_zip, copy_directory_contents, remove_directory
 
 
 DOWNLOAD_PAGE = "https://www.videolan.org/vlc/download-windows.html"
@@ -57,9 +58,15 @@ def main():
 
     print("Download complete.")
     print("Extracting VLC media player...")
-    vlc_dir = os.path.join(app_dir, "vlc_media_player")
+    vlc_dir = os.path.join(app_dir, "vlc_media_player_tmpextract")
     extract_zip(destination, vlc_dir)
     print("Extraction complete!")
+    print("Moving VLC folder...")
+    real_vlc_dir = os.path.join(app_dir, "vlc_mp")
+    copy_directory_contents(os.path.join(vlc_dir, os.listdir(vlc_dir)[0]), real_vlc_dir)
+    print("Cleaning up...")
+    remove_directory(vlc_dir)
+    print("Finished!!!")
 
 if __name__ == "__main__":
     main()
